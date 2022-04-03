a7a42106c6bc95d173413ebfcec3bee956749ca3
Bruno <brunocosta.1991@hotmail.com>
2022-04-03 19:09:27 -0300

    ngOnChanges

--- primeiro_app/src/app/title/title.component.ts
+++ primeiro_app/src/app/title/title.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
 })
-export class TitleComponent implements OnInit {
-  public title: string = 'Bem vindo';
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = 'Bem vindo';
 
   constructor() {}
-
   ngOnInit(): void {}
+  ngOnChanges(): void {
+    alert('Foi alterado com sucesso');
+  }
 }
