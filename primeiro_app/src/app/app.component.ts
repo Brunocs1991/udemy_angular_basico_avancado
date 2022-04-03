d32d1365cd12e34606af9f798643b75a873c61ca
Bruno <brunocosta.1991@hotmail.com>
2022-04-03 19:00:55 -0300

    ng oninit

--- primeiro_app/src/app/app.component.ts
+++ primeiro_app/src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log(1);
+    }, 5000);
+  }
+}
