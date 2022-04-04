a60e29d36456a32315bb5ab20c91c7debfb10ff5
Bruno <brunocosta.1991@hotmail.com>
2022-04-03 22:44:21 -0300

    ng class

--- primeiro_app/src/app/direativas-atributos/direativas-atributos.component.ts
+++ primeiro_app/src/app/direativas-atributos/direativas-atributos.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-direativas-atributos',
   templateUrl: './direativas-atributos.component.html',
-  styleUrls: ['./direativas-atributos.component.scss']
+  styleUrls: ['./direativas-atributos.component.scss'],
 })
 export class DireativasAtributosComponent implements OnInit {
+  public valor: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.valor = !this.valor;
+    }, 2000);
   }
-
 }
