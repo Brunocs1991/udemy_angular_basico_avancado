7e8379c092e9eb20b0caad837b0aaed39bdb624b
Bruno <brunocosta.1991@hotmail.com>
2022-04-06 19:58:06 -0300

    delete one and delete all takslists

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -7,9 +7,23 @@ import { TaskList } from '../../model/task-list';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = [
+    { task: 'Minha taks de teste', checked: true },
+    { task: 'Minha taks de teste2', checked: false },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public deleteItemTaksList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaksList() {
+    const confirm = window.confirm('Você deseja realmente deletar tudo?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
