8ca3bb6567a279dc4ac460e6177f8ca0f9fb9b4c
Bruno <brunocosta.1991@hotmail.com>
2022-04-08 20:12:44 -0300

    private

--- primeiro_app/src/app/services/food-list.service.ts
+++ primeiro_app/src/app/services/food-list.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class FoodListService {
   public emitEvent = new EventEmitter();
-
+ private
   private url: string = 'http://localhost:3000/';
   constructor(private http: HttpClient) {}
 
