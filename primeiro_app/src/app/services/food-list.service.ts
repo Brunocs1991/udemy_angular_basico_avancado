2ab8eba7f68fca3966e99256c222d19f2b84353c
Bruno <brunocosta.1991@hotmail.com>
2022-04-10 20:14:46 -0300

    header

--- primeiro_app/src/app/services/food-list.service.ts
+++ primeiro_app/src/app/services/food-list.service.ts
@@ -1,5 +1,5 @@
 import { FoodList } from './../module/food-list';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class FoodListService {
   public emitEvent = new EventEmitter();
  private httpOptions(){
-header:
+header:new HttpHandler
  }
   private url: string = 'http://localhost:3000/';
   constructor(private http: HttpClient) {}
