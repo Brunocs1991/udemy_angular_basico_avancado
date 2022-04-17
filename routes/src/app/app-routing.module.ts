88a433f6b27f0ad15889e901275d5210e023ee83
Bruno <brunocosta.1991@hotmail.com>
2022-04-17 19:21:35 -0300

    rotas

---
+++ routes/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+  {
+    path: '404',
+    component: PageErrorComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ routes/src/app/pages/pages.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { PageErrorComponent } from './page-error/page-error.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    SobreComponent,
+    PageErrorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
